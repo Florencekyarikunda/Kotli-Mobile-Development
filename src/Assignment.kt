20f8b8c5ebdf7dac50970ae3016dd4db9a03edc5
Florencekyarikunda <florencekyarikund@gmail.com>
2021-02-28 11:15:52 +0300

    second commit

---
+++ src/Assignment.kt
@@ -0,0 +1,30 @@
+fun main(){
+println("Hello Florence")
+    division()
+    addition()
+    print(false)
+
+
+
+}
+fun florence(){
+    println("Hello Florence")
+}
+fun division(){
+    var f = 29
+    var g =4
+    var division = f/g
+    println(division)
+}
+fun addition(){
+    var num1=2
+    var num2=6
+    var num3=12
+    var num4=14
+    var sum=num1+num2+num3+num4
+    println(sum)
+}
+fun me(){
+    var me=("Florence is very small")
+    println(false)
+}
